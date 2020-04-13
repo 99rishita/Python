afb0ddb372435093192cf906d6be19f97c72ed6e
pinninti revati <pinnintirevati999@gmail.com>
2020-04-13 19:19:32 +0530

    python practice

--- stack/next_greater.py
+++ stack/next_greater.py
@@ -1,12 +1,11 @@
 def greater(arr):
     for i in range(0,len(arr)):
+        next=-1
         for j in range(i+1, len(arr)):
-            next=-1
             if(arr[j]>arr[i]):
                 next=arr[j]
-                print(arr[j])
                 break
-            else:
-                print(next) 
-arr=[1,3,2,4]
+        print(next)
+arr=[4,3,2,1]
+greater(arr)
 
---
+++ stack/next_larger.py
@@ -0,0 +1,39 @@
+from collections import deque
+class Stack:
+    def __init__(self):
+        self.stack=[]
+    def isEmpty(self):
+        return self.stack==[]
+    def push(self,item):
+        self.stack.append(item)
+    def pop(self):
+        return self.stack.pop()
+    def peek(self):
+        return self.stack[len(self.stack)-1]
+    def size(self):
+        return len(self.stack)
+s = Stack()
+
+def larger(arr):
+    s.push(arr[0])
+    for i in range(1,len(arr)):
+        next=arr[i]
+        if(s.isEmpty()==False):
+            x=s.pop()
+            while(next>x):
+                print(next)
+                if(s.isEmpty()==True):
+                    break
+                x=s.pop()
+            if(x>next):
+                s.push(x)
+        s.push(next)
+    while(s.isEmpty()==False):
+        x = s.pop()
+        next=-1
+        print(next)
+arr=[4,3,2,1]
+larger(arr)
+            
+            
+
